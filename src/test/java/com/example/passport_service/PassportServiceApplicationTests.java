1d9c62fa29cd5a34b300c64b7600ac34516efff5
Denis.Viskov <Denis.Viskov@nordigy.ru>
2022-01-20 09:19:16 +0300

    Initial commit

---
+++ src/test/java/com/example/passport_service/PassportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.passport_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassportServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
