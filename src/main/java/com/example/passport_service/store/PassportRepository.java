67db63946fb1012a1420daf250767e7ef98f3e82
Denis.Viskov <Denis.Viskov@nordigy.ru>
2022-01-22 20:07:44 +0300

    repository

---
+++ src/main/java/com/example/passport_service/store/PassportRepository.java
@@ -0,0 +1,22 @@
+package com.example.passport_service.store;
+
+import com.example.passport_service.domain.Passport;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PassportRepository extends JpaRepository<Passport, Long> {
+    List<Passport> findPassportsBySerial(final Long serial);
+
+    @Query("select p from Passport p where p.expiredDate < current_date")
+    List<Passport> findPassportsWithExpiredDate();
+
+    @Query(
+        value = "select * from Passport p where current_date between (p.expiredDate - interval '3 month') and p.expiredDate",
+        nativeQuery = true
+    )
+    List<Passport> findReplaceablePassports();
+}
