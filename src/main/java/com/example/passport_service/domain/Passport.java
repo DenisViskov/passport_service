f29fe0dbf6c02663c19f9498f1ff9c9cbfe7a977
Denis.Viskov <Denis.Viskov@nordigy.ru>
2022-01-22 14:42:04 +0300

    migration

--- src/main/java/com/example/passport_service/domain/Passport.java
+++ src/main/java/com/example/passport_service/domain/Passport.java
@@ -1,40 +1,45 @@
 package com.example.passport_service.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.Date;
 
+@Data
 @Entity
 @Table(name = "passport")
+@NoArgsConstructor
 public class Passport {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "serial")
+    @Column(name = "serial", nullable = false)
     private Long serial;
 
-    @Column(name = "number", unique = true)
+    @Column(name = "number", unique = true, nullable = false)
     private Long number;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "surname")
+    @Column(name = "surname", nullable = false)
     private String surname;
 
     @Temporal(TemporalType.DATE)
-    @Column(name = "birth_date")
+    @Column(name = "birth_date", nullable = false)
     private Date birthDate;
 
-    @Column(name = "issuing_authority")
+    @Column(name = "issuing_authority", nullable = false)
     private String issuingAuthority;
 
     @Temporal(TemporalType.DATE)
-    @Column(name = "date_of_issue")
+    @Column(name = "date_of_issue", nullable = false)
     private Date dateOfIssue;
 
     @Temporal(TemporalType.DATE)
-    @Column(name = "expired_date")
+    @Column(name = "expired_date", nullable = false)
     private Date expiredDate;
 }
---
+++ src/main/resources/db/V1__CREATE_TABLE.sql
@@ -0,0 +1,27 @@
+-- Drop table passport
+drop table if exists passport;
+
+
+-- Create table passport
+create table passport
+(
+    id                bigserial    not null,
+    serial            int8         not null,
+    number            int8         not null,
+    name              varchar(30)  not null,
+    surname           varchar(30)  not null,
+    birth_date        date         not null,
+    issuing_authority varchar(255) not null,
+    date_of_issue     date         not null,
+    expired_date      date         not null,
+
+    -- constraints
+    constraint PASSPORT_ID_PK primary key (id),
+    constraint PASSPORT_NUMBER_UNIQUE unique (number),
+    constraint PASSPORT_CHECK_BIRTH_DATE check (birth_date > TO_DATE('01.01.1920', 'dd.mm.yyyy'))
+)
+
+
+-- Create indexes on id, serial
+create unique index PASSPORT_ID_UNIQUE_INDEX on passport (id);
+create index PASSPORT_SERIAL_INDEX on passport (serial);
